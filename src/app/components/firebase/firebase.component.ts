d95bd8f9932ff4d5eb8fab6eba7343342b6a39cc
ElishaBartov <elisha.bartov@emerson.com>
2022-10-14 01:27:04 +0300

    Added function to insert one row to firestore.

--- src/app/components/firebase/firebase.component.ts
+++ src/app/components/firebase/firebase.component.ts
@@ -19,8 +19,39 @@ export class FirebaseComponent implements OnInit,OnDestroy {
   }  
 
   ngOnInit(): void {
+
+    // Example to write one image to firestore !!!
+    this.createDocumentByCollection('images','1',{
+      author: "Alejandro Escamilla",
+      width: 5616,
+      height: 3744,
+      url: "https://unsplash.com/photos/LNRyGwIJr5c",
+      download_url: "https://picsum.photos/id/1/5616/3744"});
+
     // listen to collection changes
     this.getCollectionInRealTime('images');
+  }
+
+  createDocumentByCollection(collection:string,document:string,data: any): void {
+    this.afs.collection(collection).doc(document).set(data)
+    .then((success) => {
+      console.log(success);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }  
+
+  // use this function to create and update document.
+  // Importent note, if document not exist the function will create document. recommendation to use update methode. 
+  createDocumentByFulPath(pathToDocument:string,data:any,merge:boolean=true): void {
+    this.afs.doc(pathToDocument).set(data,{merge:merge})
+    .then((success) => {
+      console.log(success);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
   // This function reports to all about changes in database.
